8300ac4cc8a7b0e80fdc3fe5be58ad8569bbe98e
Morgan Estes <morgan@golftailor.com>
2014-04-30 10:32:42 -0500

    Updating plugin header.

--- mm-shipping-options.php
+++ mm-shipping-options.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name: Mm-shipping-options
+Plugin Name: Custom Shipping Options for MemberMouse
 Version: 0.1-alpha
-Description: PLUGIN DESCRIPTION HERE
-Author: YOUR NAME HERE
-Author URI: YOUR SITE HERE
-Plugin URI: PLUGIN SITE HERE
+Description: Adds a filter to the MemberMouse WordPress plugin to show/hide shipping options on a checkout form.
+Author: Morgan Estes
+Author URI: http://www.morganestes.com
+Plugin URI: https://github.com/morganestes/mm-shipping-options
 Text Domain: mm-shipping-options
 Domain Path: /languages
 */
