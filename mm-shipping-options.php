a8c4d722f86ec74dcf0a5c61c8999701cd0b809c
Morgan Estes <morgan@golftailor.com>
2014-04-30 10:29:24 -0500

    Inital commit.

---
+++ mm-shipping-options.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Mm-shipping-options
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: mm-shipping-options
+Domain Path: /languages
+*/
+
